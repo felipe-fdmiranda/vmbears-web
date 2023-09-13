7b543ec7465b3132c518a80d694c8b357d89c267
felipemiranda <felipemiranda@jfrn.jus.br>
2023-09-13 02:03:31 -0300

    ajustes visuais e adição de mensagem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {PaginaInicialComponent} from './pages/pagina-inicial/pagina-inicial.component';
 import {SharedModule} from "./shared/shared.module";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import {SharedModule} from "./shared/shared.module";
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pagina-inicial/pagina-inicial.component.ts
+++ src/app/pages/pagina-inicial/pagina-inicial.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {LeitorService} from "../../shared/services/leitor.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-pagina-inicial',
@@ -11,7 +12,8 @@ export class PaginaInicialComponent implements OnInit {
   enviando = false;
   files: File[] = [];
 
-  constructor(private leitorService: LeitorService) {
+  constructor(private leitorService: LeitorService,
+              private _snackBar: MatSnackBar) {
   }
 
   ngOnInit(): void {
@@ -36,21 +38,29 @@ export class PaginaInicialComponent implements OnInit {
         this.removerArquivoLido();
         if (this.files.length !== 0) {
           this.enviarProximoDaListaParaLeitura();
+        } else {
+          this.enviando = false;
+          this.mostrarMensagem("Todos os arquivos foram processados com sucesso.", false);
         }
       },
       error: (erro) => {
-        console.error(erro.error.message);
+        console.error(erro.error.message)
+        this.mostrarMensagem("Erro ao processar o arquivo XML.", true);
         this.enviando = false;
-      },
-      complete: () => {
-        console.log("Todos os arquivos foram processados com sucesso.");
-        this.enviando = false
       }
     });
   }
 
   removerArquivoLido() {
-    console.log("Arquivo " + this.files[0].name + " processado...");
     this.files.splice(0, 1);
+  }
+
+  mostrarMensagem(mensagem: string, erro: boolean) {
+    this._snackBar.open(mensagem, 'X', {
+      duration: 5000,
+      verticalPosition: 'top',
+      horizontalPosition: 'right',
+      panelClass: erro ? 'erro-snackbar' : 'success-snackbar',
+    });
   }
 }
